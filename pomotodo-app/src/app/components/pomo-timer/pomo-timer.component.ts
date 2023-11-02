7a1d2ab79dc099d56238a7f1bae56d2f57e2a03b
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2023-11-02 16:24:55 +0300

    changes : timer componenti düzenlendi.

--- pomotodo-app/src/app/components/pomo-timer/pomo-timer.component.ts
+++ pomotodo-app/src/app/components/pomo-timer/pomo-timer.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { FirebaseService } from 'src/app/shared/services/firebase.service';
 import { Subscription, interval } from 'rxjs';
-import { map, takeWhile, tap } from 'rxjs/operators';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { Title } from '@angular/platform-browser';
+import { map, take, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pomo-timer',
@@ -14,34 +12,28 @@ import { Title } from '@angular/platform-browser';
 export class PomoTimerComponent {
   loader: boolean = false;
   pomotodoTimer: string;
+  selectedTime: number = 0;
   minutes: number = 0;
-  seconds: number = 4;
+  seconds: number = 0;
   timerStopped: boolean = false;
   pomodotoInput: string = '';
   pomotodosArray: any[] = [];
   startedTime: any;
   endedTime: any;
   timerSubscription: Subscription;
+  totalTimeInSeconds: number = 0;
 
-  constructor(
-    private authService: AuthService,
-    private firebaseService: FirebaseService,
-    private activatedRoute: ActivatedRoute,
-    private title: Title,
-    private router: Router
-  ) {
-    window.addEventListener('visibilitychange', () => { this.checkContinuePomotodo() })
+  constructor(private authService: AuthService, private firebaseService: FirebaseService) {
+    window.addEventListener('visibilitychange', () => {
+      // this.checkContinuePomotodo();
+    });
   }
 
   ngOnInit(): void {
-    this.updateDisplay();
-    this.checkContinuePomotodo();
+    // this.checkContinuePomotodo();
     this.getPomotodoData();
   }
 
-  ngOnDestroy() {
-    this.checkContinuePomotodo();
-  }
 
   getPomotodoData() {
     this.loader = true;
@@ -50,7 +42,10 @@ export class PomoTimerComponent {
       .subscribe(
         (data: any) => {
           const allDatas = data.payload.data();
+          this.selectedTime = allDatas.pomotodoTime;
+          this.totalTimeInSeconds = allDatas.pomotodoTime * 60;
           this.pomotodosArray = allDatas.pomotodos.reverse();
+          this.pomotodoTimer = `${allDatas.pomotodoTime}:00`
           this.loader = false;
         },
         (err) => {
@@ -62,82 +57,67 @@ export class PomoTimerComponent {
 
 
   startTimer() {
-    this.timerStopped = true;
     this.startedTime = new Date().getTime();
-    this.endedTime = this.startedTime + (this.minutes * 60 * 1000 + this.seconds * 1000);
-    // Local Storage
-    localStorage.setItem('pomodoroStartTime', this.startedTime.toString());
-    localStorage.setItem('pomodoroEndTime', this.endedTime.toString());
+    this.endedTime = this.startedTime + this.totalTimeInSeconds * 1000;
+    this.timerStopped = true;
 
-    const interval$ = interval(1000);
+    this.setPomotodoLocalStorage();
 
-    this.timerSubscription = interval$.pipe(
-      map((tick) => this.endedTime - new Date().getTime()),
-      takeWhile((remainingTime) => remainingTime > 0),
-      tap((remainingTime) => {
-        this.minutes = Math.floor(remainingTime / (60 * 1000));
-        this.seconds = Math.floor((remainingTime % (60 * 1000)) / 1000);
-        this.updateDisplay();
-      })
-    ).subscribe({
-      complete: () => {
-        console.log('Timer bitti');
-        this.pomotodoTimer = 'Pomodoto Name?';
-      }
-    });
+    this.timerSubscription = interval(1000)
+      .pipe(
+        map((timer) => this.totalTimeInSeconds - timer),
+        takeWhile((timer) => timer >= 0),
+        take(1500)
+      )
+      .subscribe((timer) => {
+        const dakika = Math.floor(timer / 60);
+        const saniyeKalan = timer % 60;
+        if (timer > 0) {
+          this.pomotodoTimer = `${dakika}:${saniyeKalan < 10 ? '0' : ''}${saniyeKalan}`;
+        } else {
+          this.userFinishedPomotodo();
+        }
+      });
   }
 
 
-  stopTimer() {
-    // Local Storage'ı temizle
+  stopTimerButton() {
+    this.clearPomotodoLocalStorage();
+    if (this.timerSubscription) {
+      this.timerStopped = false;
+      this.pomotodoTimer = `${this.selectedTime}:00`;
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+
+  userFinishedPomotodo() {
+    this.pomotodoTimer = 'Pomodoto Name?';
+  }
+
+
+  setPomotodoLocalStorage() {
+    if (typeof this.startedTime === 'undefined' || typeof this.endedTime === 'undefined') {
+      return;
+    }
+    const startTime = Number(localStorage.getItem('pomodoroStartTime'));
+    if (!startTime) {
+      localStorage.setItem('pomodoroStartTime', this.startedTime.toString());
+      localStorage.setItem('pomodoroEndTime', this.endedTime.toString());
+    }
+  }
+
+
+  clearPomotodoLocalStorage() {
     localStorage.removeItem('pomodoroStartTime');
     localStorage.removeItem('pomodoroEndTime');
-    console.log("ok");
-
-    // RxJS 
-    if (this.timerSubscription) {
-      console.log("rxjs")
-      this.timerStopped = false;
-      this.timerSubscription.unsubscribe();
-      this.clearInterval();
-      this.updateDisplay();
-    }
   }
 
-  clearInterval() {
-    this.minutes = 25;
-    this.seconds = 0;
-    this.updateDisplay();
-  }
-
-  updateDisplay() {
-    this.pomotodoTimer = `${this.pad(this.minutes)}:${this.pad(this.seconds)}`;
-  }
-
-  pad(value: number): string {
-    return value < 10 ? '0' + value : value.toString();
-  }
-
-  checkContinuePomotodo() {
-    const startTime = Number(localStorage.getItem('pomodoroStartTime'));
-    const endTime = Number(localStorage.getItem('pomodoroEndTime'));
-    if (startTime && endTime) {
-      const currentTime = new Date().getTime();
-      console.log("ok");
-
-      if (currentTime < endTime) {
-        const remainingTime = endTime - currentTime;
-        this.minutes = Math.floor(remainingTime / 60000);
-        this.seconds = Math.floor((remainingTime % 60000) / 1000);
-        this.startTimer();
-      }
-    }
-  }
 
   enterPressed(event: KeyboardEvent) {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       this.savePomotodo(); //
-      this.stopTimer();
+      this.stopTimerButton();
       this.timerStopped = false;
     }
   }
@@ -145,22 +125,23 @@ export class PomoTimerComponent {
   savePomotodo() {
     const startTime = Number(localStorage.getItem('pomodoroStartTime'));
     const endTime = Number(localStorage.getItem('pomodoroEndTime'));
-
     const pomodotoData = {
       name: this.pomodotoInput,
       startTime: new Date(startTime),
-      endTime: new Date(endTime)
+      endTime: new Date(endTime),
     };
 
     this.pomotodosArray.push(pomodotoData);
 
     let pomotodoData = {
-      pomotodos: this.pomotodosArray
-    }
+      pomotodos: this.pomotodosArray,
+    };
 
-    this.firebaseService.UpdateFirebaseData('/users/', this.authService.userUid, pomotodoData)
+    this.firebaseService
+      .UpdateFirebaseData('/users/', this.authService.userUid, pomotodoData)
       .then(() => {
-        this.stopTimer();
+        this.stopTimerButton();
+        this.pomodotoInput = '';
       })
       .catch((error) => {
         console.error('hata oluştu: ' + error);
@@ -168,10 +149,57 @@ export class PomoTimerComponent {
   }
 
 
+  checkContinuePomotodo() {
+    const startTime = Number(localStorage.getItem('pomodoroStartTime'));
+    const endTime = Number(localStorage.getItem('pomodoroEndTime'));
+    if (startTime && endTime) {
+      // this.startPomotodoTimer();
+    }
+  }
+
+
+  // startPomotodoTimer() {
+  //   const storedStartTime = Number(localStorage.getItem('pomodoroStartTime'));
+  //   const storedEndTime = Number(localStorage.getItem('pomodoroEndTime'));
+
+  //   if (storedStartTime && storedEndTime) {
+  //     const currentTime = new Date().getTime();
+  //     const remainingTimeInSeconds = Math.floor((storedEndTime - currentTime) / 1000);
+
+  //     if (remainingTimeInSeconds > 0) {
+  //       this.totalTimeInSeconds = remainingTimeInSeconds;
+  //       this.startedTime = storedStartTime;
+  //       this.endedTime = storedEndTime;
+  //       this.timerStopped = true;
+  //     } else {
+  //       this.totalTimeInSeconds = this.minutes * 60 + this.seconds;
+  //       this.startedTime = new Date().getTime();
+  //       this.endedTime = this.startedTime + this.totalTimeInSeconds * 1000;
+  //       this.timerStopped = true;
+
+  //       // Yerel depoya başlama ve bitiş zamanlarını kaydet
+  //       this.setPomotodoLocalStorage();
+  //     }
+
+  //     this.timerSubscription = interval(1000)
+  //       .pipe(
+  //         map((timer) => this.endedTime - new Date().getTime() - timer * 1000),
+  //         takeWhile((timer) => timer >= 0),
+  //         take(1500)
+  //       )
+  //       .subscribe((timer) => {
+  //         const dakika = Math.floor(timer / 60);
+  //         const saniyeKalan = timer % 60;
+  //         if (timer > 0) {
+  //           console.log(`${dakika}:${saniyeKalan < 10 ? '0' : ''}${saniyeKalan}`);
+  //         } else {
+  //           console.log("Süre bitti!");
+  //         }
+  //       });
+  //   }
+  // }
+
+
+
+
 }
-
-
-
-
-
-
